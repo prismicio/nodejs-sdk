105139dc93263a52155f42e8755266a22fb1810d
Erwan Loisant <eloisant@gmail.com>
2016-05-03 15:12:33 +0200

    Revert repository name

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  apiEndpoint: 'https://testerw.prismic.io/api',
+  apiEndpoint: 'https://your-repo-name.prismic.io/api',
 
   // -- Access token if the Master is not open
   // accessToken: 'xxxxxx',

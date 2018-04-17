513c397640033a7901f32371c4b36e5815177378
Levi Gable <levimykel@gmail.com>
2018-04-17 15:23:43 +0200

    Fix linting issues

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -14,7 +14,7 @@ module.exports = {
   // As your project grows, you should update this function according to your routes
   linkResolver(doc) {
     if (doc.type === 'page') {
-      return '/page/' + doc.uid;
+      return `/page/${doc.uid}`;
     }
 
     return '/';
